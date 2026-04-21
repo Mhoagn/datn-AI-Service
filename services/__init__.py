d49e43c28aa88c2e509a174136118837145c9db9
MinhHoang <minhhoangdayyyy07@gmail.com>
2026-04-21 15:02:59 +0700

    update gitignore

--- services/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Services package
-"""
-from .video_processor import VideoProcessor
-from .transcript_service import TranscriptService
-from .summary_service import SummaryService
-
-__all__ = ['VideoProcessor', 'TranscriptService', 'SummaryService']
